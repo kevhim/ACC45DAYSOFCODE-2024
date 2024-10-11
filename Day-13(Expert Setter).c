6fbd9e9d061605acb52299b789b8426789b4c256
kevhim <kevinhatoshi@gmail.com>
2024-10-11 22:12:51 +0530

    Update Day-13(Expert Setter).c

--- Day-13(Expert Setter).c
+++ Day-13(Expert Setter).c
@@ -3,16 +3,12 @@
 int main() {
     int T; 
     
- 
     scanf("%d", &T);
-    
   
     for (int i = 0; i < T; i++) {
         int X, Y; 
         
-       
         scanf("%d %d", &X, &Y);
-        
     
         if (Y >= X / 2.0) {
             printf("Yes\n");
